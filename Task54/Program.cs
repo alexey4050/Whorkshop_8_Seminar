2c9dcf1e026ce716831738805f39ce3c09afc76e
alexey <alexey4050@mail.ru>
2022-09-04 17:18:19 +0300

    Решение задачи 54

---
+++ Task54/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 54: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Clear();
+Console.WriteLine($"Задача 54. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива. ");
+
+int [,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int [m, n];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    } 
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],3},");
+            else Console.Write($"{array[i, j],3} ]");
+        }
+        Console.WriteLine();
+    }   
+}
+
+void OrderArrayLines (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array [i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+
+                }
+            }
+        }
+    }
+}
+
+int[,] array = GetMatrix(3, 4);
+Console.WriteLine($"Массив, заполненный случайными числами: ");
+PrintMatrix(array);
+Console.WriteLine();
+Console.WriteLine($"Отсортированный массив:");
+OrderArrayLines(array);
+PrintMatrix(array);
